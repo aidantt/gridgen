4629ccc8567d028b55947dee6be7defca9c35dc1
aidantt <aidant7376@gmail.com>
2026-03-05 15:47:35 -0600

    core update loop in progress

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 #    using Gauss-Seidel to obtain an improved orthogonal grid within tolerance
 
 # module imports
-from boundary_conditions import *
+from boundary_conditions import xL, xR, yL, yU
 from visualization import plot_grid
 
 # library imports
@@ -18,33 +18,61 @@ import matplotlib.pyplot as plt # for grid visualization
 # Step 1: initialize values ####################################################
 
 # define Imax, Jmax, and tolerance
-Imax = 5
-Jmax = 5
+# i=j=0 is the upper left corner
+# so, i increases downwards and j increases to the right
+Imax = 6
+Jmax = 11
 
 tolerance = 1e-6
+max_iter = 1e6
 
 # Step 2: construct algebraic grid #############################################
 
 # define mapping from (i,j) to (xi,eta) as equal spacings
-xi, eta = np.meshgrid(np.linspace(0,1,Imax), np.linspace(0,1,Jmax))
+# xi=eta=0 at the upper left corner
+# xi increases to the right, eta increases downwards
+xi, eta = np.meshgrid(np.linspace(0,1,Jmax), np.linspace(0,1,Imax))
 
 # define algebraic mapping from (xi,eta) to (x,y)
 x = xL + xi*(xR-xL)
 y = yL(x) + eta*(yU(x) - yL(x))
 
 # plot the x,y grid using the algebraic mapping
-#plot_grid(x, y)
+# plot_grid(xi, eta)
+# plot_grid(x, y)
 
 # Step 3: solve laplacian using Gauss-Seidel ###################################
 
 # from the definition of xi, eta, calculate the discretized \Delta xi and eta
-d_xi = 1/(Imax-1)
-d_eta = 1/(Jmax-1)
+d_xi = 1/(Jmax-1)
+d_eta = 1/(Imax-1)
 
 # test the definitions above
-#print(f'd_xi = {d_xi}, d_eta = {d_eta}')
-#plot_grid(xi, eta)
+# print(f'd_xi = {d_xi}, d_eta = {d_eta}')
 
-# cast a grid x(i,j) into a 1D array of dimension Imax*Jmax
-x_l = x.flatten(order='F') # column-major order
+# print(eta)
+# print(eta.shape)
 
+# initialize values for the iteration loop
+iteration = 1
+error = np.inf
+alpha = np.zeros_like(x)
+beta = np.zeros_like(x)
+gamma = np.zeros_like(x)
+x_old = x.copy()
+y_old = y.copy()
+
+while (error > tolerance) or (iteration < max_iter):
+    # loop through the interior points of the grid
+    for i in range(1, Imax-1):
+        for j in range(1, Jmax-1):
+            # calculate alpha, beta, gamma at the current point (i,j) using the definitions
+            alpha[i,j] = 1/(4*d_eta**2) * \
+                ( (x_old[i,j+1] - x_old[i,j-1])**2 + (y_old[i,j+1] - y_old[i,j-1])**2 )
+            
+            beta[i,j] = 1/(4*d_xi*d_eta) * \
+                ( (x_old[i+1,j] - x_old[i-1,j])*(x_old[i,j+1] - x_old[i,j-1]) + \
+                 (y_old[i+1,j] - y_old[i-1,j])*(y_old[i,j+1] - y_old[i,j-1]) )
+            
+            gamma[i,j] = 1/(4*d_xi**2) * \
+                ( (x_old[i+1,j] - x_old[i-1,j])**2 + (y_old[i+1,j] - y_old[i-1,j])**2 )
--- visualization.py
+++ visualization.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 # define a function to plot a grid given a set of arrays
